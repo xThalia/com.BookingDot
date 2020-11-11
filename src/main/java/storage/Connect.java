a800b77d489dfc02a5d0705e171662de80289302
NataliaM <m_natalia>
2020-11-12 00:34:35 +0100

    Try database

--- src/main/java/storage/Connect.java
+++ src/main/java/storage/Connect.java
@@ -3,6 +3,7 @@ import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connect {
     public static void connect() {
@@ -30,10 +31,32 @@ public class Connect {
             }
         }
     }
+
+    public static void createUserTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:src.main.java.storage.bookingDB.db";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS user (\n" +
+    " username VARCHAR(16) NOT NULL, \n"+
+  "email VARCHAR(255) NULL,\n" +
+  "password VARCHAR(32) NOT NULL,\n" +
+  "create_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP);\n";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Super, znowu sie udało");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args){
         connect();
+        createUserTable();
     }
 }
