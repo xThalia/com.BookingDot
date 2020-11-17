dac7b8eb437756abe6e20f86aaa4d2bf6b9a9a02
GinP <slawekf6@o2.pl>
2020-11-18 00:45:18 +0100

    LOGIN

--- src/main/java/pl/servlets/auth/LoginController.java
+++ src/main/java/pl/servlets/auth/LoginController.java
@@ -1,9 +1,12 @@
 package pl.servlets.auth;
 
+import providers.UserDataProvider;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,7 +19,22 @@ public class LoginController extends HttpServlet {
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        PrintWriter writer = response.getWriter();
-        writer.println("XD");
+       String login    = request.getParameter("email");
+       String password = request.getParameter("password");
+       int id          = 0;
+       UserDataProvider userDataProvider = new UserDataProvider();
+
+       System.out.println(login + " password " + password);
+       if (userDataProvider.authenticateUserWithLoginAndPassword(login, password) != 0) {
+           System.out.println("ok2");
+           id = userDataProvider.authenticateUserWithLoginAndPassword(login, password);
+           HttpSession session = request.getSession(true);
+           session.setAttribute("currentSessionUser",id);
+           response.sendRedirect("testredirect.jsp");
+       } else {
+           System.out.println("nie ok");
+           request.getSession().setAttribute("status", "error");
+           response.sendRedirect("views/auth/login.jsp");
+        }
     }
 }
